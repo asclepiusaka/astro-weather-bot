0c12da4b8f9c09221de7cd159d6c75c8e5d1df72
saka <asclepius1993@gmail.com>
2017-07-26 16:00:12 -0400

    setup webhook

---
+++ astro-weather-bot/bot.py
@@ -0,0 +1,52 @@
+import telepot
+from telepot.loop import OrderedWebhook
+import sys
+from flask import Flask, request
+import urllib.parse
+import urllib.request
+
+"""
+python bot.py [port]
+"""
+
+def handler(msg):
+    global bot
+    if 'location' not in msg['message']:
+        pass
+    else:
+        location = msg['message']['location']
+        base_api_url = 'http://202.127.24.18/v4/bin/astro.php?'
+        param = {'lon':location['longitude'],'lat':location['latitude'],'output':'internal'}
+        query_url = base_api_url+urllib.parse.urlencode(param)
+        bot.sendPhoto(msg['message']['chat']['id'],('astro.png',urllib.request.urlopen(query_url)))
+
+
+with open('token','r') as f:
+    token = f.readline()
+
+token = token.strip('/n')
+bot = telepot.Bot(token)
+port = int(sys.argv[1])
+url = 'http://saka.mdzz.info/'+token
+
+app = Flask(__name__)
+
+webhook = OrderedWebhook(bot,handler)
+
+@app.route(url,methods=['POST','GET'])
+def pass_update():
+    webhook.feed(request.data)
+    return 'OK'
+
+if __name__=='__main__':
+    try:
+        bot.setWebhook(url)
+    except telepot.exception.TooManyRequestsError:
+        pass
+
+    webhook.run_as_thread()
+    app.run(port=port,debug=True)
+
+
+
+
